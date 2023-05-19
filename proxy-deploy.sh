a0c1e84ee5fbb4b55be8d6f2781a552479a738b3
AndoniHQ <andoni.hernandez.quero@gmail.com>
2023-05-19 18:54:14 +0200

    Root user anticipation

--- proxy-deploy.sh
+++ proxy-deploy.sh
@@ -2,8 +2,11 @@
 
 #Comprobaciones previas
 set -eu -o pipefail # Interrumpe la ejecucion cuando haya un error y debugealo
-sudo -n true
-test $? -eq 0 || exit 1 "Debes tener privilegios sudo para ejecutar este script"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Debes ejecutar el comando como root."
+  exit
+fi
 
 if [[ $# -eq 0 ]] ; then
     echo -e "Debes indicar el nombre del cliente. Ejemplo:\n"
@@ -19,36 +22,36 @@ AGENT_CONFIG="/etc/zabbix/zabbix_agentd.conf"
 
 #Acciones
 echo -e "\n[*]Agregando repositorios de Zabbix...\n"
-sudo wget "${ZABBIX_URL}" -P /tmp/
-sudo dpkg -i /tmp/zabbix-release_6.0-4+ubuntu22.04_all.deb
-sudo apt update
+wget "${ZABBIX_URL}" -P /tmp/
+dpkg -i /tmp/zabbix-release_6.0-4+ubuntu22.04_all.deb
+apt update
 
 echo -e "\n[*]Instalando requisitos...\n"
-sudo apt install -y resolvconf wireguard zabbix-proxy-sqlite3 zabbix-agent
+apt install -y resolvconf wireguard zabbix-proxy-sqlite3 zabbix-agent
 
 echo -e "\n[*]Configurando Wireguard...\n"
-sudo mv "${CLIENT}-Proxy.conf" "/etc/wireguard/${CLIENT}.conf"
-sudo chmod 600 "/etc/wireguard/${CLIENT}.conf"
+mv "${CLIENT}-Proxy.conf" "/etc/wireguard/${CLIENT}.conf"
+chmod 600 "/etc/wireguard/${CLIENT}.conf"
 
 echo -e "\n[*]Iniciando servicio VPN...\n"
-sudo service wg-quick@$CLIENT start
-sudo systemctl enable wg-quick@$CLIENT
+service wg-quick@$CLIENT start
+systemctl enable wg-quick@$CLIENT
 sleep 5
 
 echo -e "\n[*]Configurando Zabbix Proxy...\n"
-sudo sed -i "s/\(Server *= *\).*/\110.10.10.1/" $PROXY_CONFIG && echo -e "\tServer=10.10.10.1"
-sudo sed -i "s/\(Hostname *= *\).*/\1$CLIENT-Proxy/" $PROXY_CONFIG && echo -e "\tHostname=$CLIENT-Proxy"
-sudo sed -i "s/\(DBName *= *\).*/\1\/tmp\/zabbix\_proxy\.db/" $PROXY_CONFIG && echo -e "\tDBName=/tmp/zabbix_proxy.db"
+sed -i "s/\(Server *= *\).*/\110.10.10.1/" $PROXY_CONFIG && echo -e "\tServer=10.10.10.1"
+sed -i "s/\(Hostname *= *\).*/\1$CLIENT-Proxy/" $PROXY_CONFIG && echo -e "\tHostname=$CLIENT-Proxy"
+sed -i "s/\(DBName *= *\).*/\1\/tmp\/zabbix\_proxy\.db/" $PROXY_CONFIG && echo -e "\tDBName=/tmp/zabbix_proxy.db"
 
 echo -e "\n[*]Iniciando servicio zabbix-proxy...\n"
-sudo service zabbix-proxy restart
-sudo systemctl enable zabbix-proxy
+service zabbix-proxy restart
+systemctl enable zabbix-proxy
 
 echo -e "\n[*]Configurando Zabbix Agent...\n"
-sudo sed -i "s/\(Hostname *= *\).*/\1$CLIENT-Proxy/" $AGENT_CONFIG && echo -e "\tHostname=$CLIENT-Proxy"
-sudo sed -i '/HostMetadata=/s/^# //' $AGENT_CONFIG
-sudo sed -i "s/\(HostMetadata *= *\).*/\1$CLIENT,Proxy,Linux/" $AGENT_CONFIG && echo -e "\tHostMetadata=$CLIENT,Proxy,Linux"
+sed -i "s/\(Hostname *= *\).*/\1$CLIENT-Proxy/" $AGENT_CONFIG && echo -e "\tHostname=$CLIENT-Proxy"
+sed -i '/HostMetadata=/s/^# //' $AGENT_CONFIG
+sed -i "s/\(HostMetadata *= *\).*/\1$CLIENT,Proxy,Linux/" $AGENT_CONFIG && echo -e "\tHostMetadata=$CLIENT,Proxy,Linux"
 
 echo -e "\n[*]Iniciando servicio zabbix-agent...\n"
-sudo service zabbix-agent restart
-sudo systemctl enable zabbix-agent
+service zabbix-agent restart
+systemctl enable zabbix-agent
